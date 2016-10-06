45cc546a37d36d7a4eeb24fbfca7836e8216e871
Ben Harling <ben.harling@mediaclash.co.uk>
2016-10-06 10:07:23 +0100

    Removed setup restriction on django 1.10 fixes #1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='django-pint',
       packages=find_packages(exclude=["sandbox*", "tests*"]),
       include_package_data=True,
       install_requires=[
-          'django>=1.8,<1.10',
+          'django>=1.8',
           'pint>=0.7.2',
           ],
       # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
