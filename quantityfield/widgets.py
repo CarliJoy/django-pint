cf35db87d6e5520a4ca4d291c8abc499b0e1cec7
Carli* Freudenberg <kound@posteo.de>
2020-11-07 19:06:14 +0100

    fix duplicated init
    Note: this mistake was not recognized by any tests!

--- quantityfield/widgets.py
+++ quantityfield/widgets.py
@@ -6,22 +6,18 @@ from . import ureg as default_ureg
 
 
 class QuantityWidget(MultiWidget):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *, attrs=None, base_units=None, allowed_types=None, **kwargs):
         self.ureg = kwargs.pop("ureg", default_ureg)
-
-    def get_choices(self, allowed_types=None):
-        allowed_types = allowed_types or dir(self.ureg)
-        return [(x, x) for x in allowed_types]
-
-    def __init__(self, attrs=None, base_units=None, allowed_types=None):
         choices = self.get_choices(allowed_types)
         self.base_units = base_units
         attrs = attrs or {}
         attrs.setdefault("step", "any")
-
         widgets = (NumberInput(attrs=attrs), Select(attrs=attrs, choices=choices))
-
         super(QuantityWidget, self).__init__(widgets, attrs)
+
+    def get_choices(self, allowed_types=None):
+        allowed_types = allowed_types or dir(self.ureg)
+        return [(x, x) for x in allowed_types]
 
     def decompress(self, value):
         non_decimal = re.compile(r"[^\d.]+")
