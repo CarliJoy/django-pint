f18206a056c8403ac173de569318093be38654d2
Carli* Freudenberg <kound@posteo.de>
2020-11-10 21:53:03 +0100

    remove not required string representation

--- tests/dummyapp/models.py
+++ tests/dummyapp/models.py
@@ -18,20 +18,11 @@ class HayBale(models.Model):
     weight_int = IntegerQuantityField("gram", blank=True, null=True)
     weight_bigint = BigIntegerQuantityField("gram", blank=True, null=True)
 
-    def __str__(self):
-        return self.name
-
 
 class EmptyHayBale(models.Model):
     name = models.CharField(max_length=20)
     weight = QuantityField("gram", null=True)
 
-    def __str__(self):
-        return self.name
-
 
 class CustomUregHayBale(models.Model):
     custom = QuantityField("custom", ureg=custom_ureg, null=True)
-
-    def __str__(self):
-        return self.name
