98ef0f366a68f28595efe8b114749caf9681dfe0
Carli* Freudenberg <kound@posteo.de>
2020-11-29 13:38:46 +0100

    do not include never executed code in coverage

--- src/quantityfield/__init__.py
+++ src/quantityfield/__init__.py
@@ -4,7 +4,9 @@ try:
     # Change here if project is renamed and does not equal the package name
     dist_name = "django-pint"
     __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+except DistributionNotFound:  # pragma: no cover
+    # We don't expect this to be executed, as this would mean the configuration
+    # for the python module is wrong
     __version__ = "unknown"
 finally:
     del get_distribution, DistributionNotFound
