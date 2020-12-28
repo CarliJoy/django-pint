280228fdb445f6267cf275fd47df366a68b08b0d
Carli* Freudenberg <kound@posteo.de>
2020-12-28 23:45:58 +0100

    create the correct database in CI

--- ci_setup_postgres.sh
+++ ci_setup_postgres.sh
@@ -1,4 +1,4 @@
-psql -c "create database django_test;" -U postgres
+psql -c "create database django_pint;" -U postgres
 # Settings done according to tutorial https://www.digitalocean.com/community/tutorials/how-to-set-up-django-with-postgres-nginx-and-gunicorn-on-ubuntu-18-04
 psql -c "CREATE USER django_pint WITH PASSWORD 'not_secure_in_testing';" -U postgres
 psql -c "ALTER ROLE django_pint SET client_encoding TO 'utf8';" -U postgres
